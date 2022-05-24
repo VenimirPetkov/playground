29e4eaf1a4a028940cced04f2bc7e0be69d796d2
VenimirPetkov <venimirp@gmail.com>
2022-05-24 16:48:03 +0300

    clean

--- test/nft-test.js
+++ test/nft-test.js
@@ -1,46 +1,12 @@
-const { expect } = require("chai");
 const { ethers } = require("hardhat");
-const Wallet = require('ethereumjs-wallet').default;
-const { fromRpcSig } = require('ethereumjs-util');
-const ethSigUtil = require('eth-sig-util');
-const web3 = require('web3');
+
 
   const name = 'Venimir NFT';
   const symbol = 'VLP';
-  const firstTokenId = "0";
-  const baseURI = 'https://api.vlp.com/v1/';
   let owner, addr1, addr2;
   let chainId;
   let venimirNftContract;
-  const version = web3.utils.toBN('1');
 
-  const EIP712Domain = [
-    { name: 'name', type: 'string' },
-    { name: 'version', type: 'string' },
-    { name: 'chainId', type: 'uint256' },
-    { name: 'verifyingContract', type: 'address' },
-  ];
-  const Permit = [
-    { name: 'from', type: 'address' },
-    { name: 'to', type: 'address' },
-    { name: 'tokenId', type: 'uint256' },
-    { name: 'nonce', type: 'uint256' },
-    { name: 'deadline', type: 'uint256' },
-  ];
-  const duration = {
-    seconds: function (val) { return new BN(val); },
-    minutes: function (val) { return new BN(val).mul(this.seconds('60')); },
-    hours: function (val) { return new BN(val).mul(this.minutes('60')); },
-    days: function (val) { return new BN(val).mul(this.hours('24')); },
-    weeks: function (val) { return new BN(val).mul(this.days('7')); },
-    years: function (val) { return new BN(val).mul(this.days('365')); },
-  };
-
-  // Returns the time of the last mined block in seconds
-  async function latest () {
-    const block = await web3.eth.getBlock('latest');
-    return block.timestamp;
-  }
   beforeEach(async function () {
     const ERC721Factory = await ethers.getContractFactory("ERC721");
     const ERC721 = await ERC721Factory.deploy(name, symbol);
